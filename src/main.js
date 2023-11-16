ca4f2c21618d7fad4fc5e655b3d8756ac8c06e80
xcvk <henrykuang11@gmail.com>
2023-11-16 02:00:08 -0500

    reopt

--- src/main.js
+++ src/main.js
@@ -1,26 +1,10 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
-const { token } = require('./config.json');
-
-const client = new Client({
+const {Client, IntentsBitField } = require('discord.js');
+const { token } = require('./config.json')
+const client = new Client ({
   intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMessages,
   ],
-});
-
-
-
-client.once(Events.ClientReady, client => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
-
-client.on('messageCreate', (message) => {
-  // Your bot's message handling code goes here
-  if (message.content === '!ping') {
-    message.reply('Pong!');
-  }
-});
+})
 
 client.login(token);
-
